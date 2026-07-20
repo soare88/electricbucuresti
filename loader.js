38ba706a8ad19f4a15af0f0cc7d67045b39ec715
Mihai Soare <soare.soare88@gmail.com>
2026-07-20 03:13:20 +0300

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Încarcă header-ul
     fetch('header.html')
         .then(response => response.text())
         .then(data => document.getElementById('header-placeholder').innerHTML = data);
     
-    // Încarcă footer-ul
     fetch('footer.html')
         .then(response => response.text())
         .then(data => document.getElementById('footer-placeholder').innerHTML = data);
